404fb1dbe056be994e5bae4f6d4eeec327afe18b
John Robertson <jjrob13@gmail.com>
2015-02-16 08:51:58 -0700

    Add module1.vhd.

--- Part 2/module1.vhd
+++ Part 2/module1.vhd
@@ -1,34 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    21:34:25 02/15/2015 
--- Design Name: 
--- Module Name:    module1 - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity module1 is
     Port ( S : in  STD_LOGIC_VECTOR (4 downto 0);
            D : out  STD_LOGIC_VECTOR (6 downto 0));
