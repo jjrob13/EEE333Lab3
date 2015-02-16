4ae58df0e86da8bc31e5e8570a08f2d98f49eeec
John Robertson <jjrob13@gmail.com>
2015-02-16 08:45:40 -0700

    Move Part 2 files to Part 2 folder and rename files to correct file names.

--- Part 2/module1_TB.vhd
+++ Part 2/module1_TB.vhd
@@ -1,44 +1,10 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   22:08:04 02/15/2015
--- Design Name:   
--- Module Name:   C:/Users/Abdulrahman/Desktop/SHAIKHA ALWEHAIBI/lab3/part2-1/module1_TB.vhd
--- Project Name:  part2-1
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: module1
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
  
 ENTITY module1_TB IS
 END module1_TB;
  
 ARCHITECTURE behavior OF module1_TB IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
     COMPONENT module1
     PORT(
          S : IN  std_logic_vector(4 downto 0);
---
+++ Part 2/module3.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity module3 is
+    Port ( S : in  STD_LOGIC;
+           D : in  STD_LOGIC);
+end module3;
+
+architecture Behavioral of module3 is
+
+begin
+
+D <= not S;
+
+end Behavioral;
+
